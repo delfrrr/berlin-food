296d8bdcfda5e6bee445faaf5d394a802bdf156d
Vova Bilonenko <bilonenko.v@gmail.com>
2015-08-21 22:57:25 +0200

    add some docs

--- fetch.js
+++ fetch.js
@@ -2,23 +2,42 @@
  * fetch data from fousquare
  */
 
+/**
+ * @typedef {array} ll
+ * @prop {number} 0 lat
+ * @prop {number} 0 lng
+ */
+
+/**
+ * @typedef {object} geojson
+ * @see http://geojson.org/geojson-spec.html
+ */
+
 var program = require('commander'),
     packagejson = require('./package.json'),
     csv = require('to-csv'),
-    turf = require('turf'),
-    CLIENT_ID = '3S4FSCICKVNFXZ2EUKRV0LIL3TS3B1JMEOXTSEVNSE1KAFWU',
-    CLIENT_SECRET = '3S4FSCICKVNFXZ2EUKRV0LIL3TS3B1JMEOXTSEVNSE1KAFWU',
-    centerLl = [52.516667, 13.383333],
-    radius = 20 * 1000;//m
+    turf = require('turf');
 
 program
     .version(packagejson.version)
+    .option('-r, --radius [number]', 'Radius km', Number, 1)
     .description('fetch data from fousquare');
 program.parse(process.argv);
+
+var CLIENT_ID = '3S4FSCICKVNFXZ2EUKRV0LIL3TS3B1JMEOXTSEVNSE1KAFWU',
+    CLIENT_SECRET = '3S4FSCICKVNFXZ2EUKRV0LIL3TS3B1JMEOXTSEVNSE1KAFWU',
+    centerLl = [52.516667, 13.383333],
+    radius = program.radius * 1000;//m
+
 
 var axis1 = [];
 var axis2 = [];
 
+/**
+ * get line geojson
+ * @param {ll[]} coordinates
+ * @return {geojson} coordinates
+ */
 function getLineByCoords(coordinates) {
     return {
         type: 'Feature',
@@ -29,6 +48,12 @@ function getLineByCoords(coordinates) {
     };
 }
 
+/**
+ * @param {array<{ll}>} collection
+ * @param {geojson} line
+ * @param {number} radius m
+ * @param {number} distance m
+ */
 function walk(collection, line, radius, distance) {
     distance = distance || 0;
     distance += 250;
@@ -44,16 +69,12 @@ walk(axis1, getLineByCoords([centerLl, [centerLl[0], 90]]), radius);
 walk(axis2, getLineByCoords([centerLl, [0, centerLl[1]]]), radius);
 walk(axis2, getLineByCoords([centerLl, [90, centerLl[1]]]), radius);
 
-var cells = [];
+var net = [];
 axis1.forEach(function (ll) {
     var lng = ll[1];
     axis2.forEach(function (ll) {
         var lat = ll[0];
-        cells.push({
-            lat: lat,
-            lng: lng,
-            value: 1
-        });
+        net.push([lat, lng]);
     });
 });
-console.log(csv(cells));
+console.log('net length %j', net.length);
