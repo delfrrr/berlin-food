8122f7a3edb9c457572398055a6b881f0e3c1357
Vova Bilonenko <bilonenko.v@gmail.com>
2015-12-16 00:37:23 +0100

    group by street and distance

--- apps/cluster.js
+++ apps/cluster.js
@@ -13,6 +13,7 @@ var Mongo = require('schema-check-mongo-wrapper');
 var connection = new Mongo.Connection('mongodb://localhost:27017/foursqare');
 var collection = connection.collection('venues');
 var soundex = require('soundex-code');
+var turf = require('turf');
 // var metaphone = require('metaphone');
 
 /**
@@ -65,34 +66,77 @@ function groupByStreetName(byStreet, venue) {
     return byStreet;
 }
 
+/**
+ * @const
+ */
+var MAX_WALK_DISTANCE = 50; //meters
+
+/**
+ * @param {Venue} venue
+ * @return {Point}
+ */
+function getPoint(venue) {
+    var location = venue.location;
+    return {
+        'type': 'Feature',
+        'properties': {},
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [location.lng, location.lat]
+        }
+    };
+}
+
+/**
+ * Callback for reduce
+ * @param {Venue[][]} groups
+ * @param {Venue} venue
+ */
+function groupByDistance(groups, venue) {
+    var newGroup = [venue];
+    //leave only groups which are not close to venue
+    groups = groups.filter(function (group) {
+        //check if any group venue close to venue
+        if (group.some(function (groupVenue) {
+            var distance = turf.distance(
+                getPoint(venue),
+                getPoint(groupVenue),
+                'kilometers'
+            ) * 1000;
+            return distance < MAX_WALK_DISTANCE;
+        })) {
+            //if close attach group to new group
+            newGroup = newGroup.concat(group);
+            return false;
+        } else {
+            return true;
+        }
+    });
+    groups.push(newGroup);
+    return groups;
+}
+
 program
     .version(packagejson.version)
     .description('clusters venues');
 program.parse(process.argv);
 
 collection.find({}).toArray().then(function (venues) {
-    var result = venues.reduce(groupByStreetName, {});
-    console.log(
-        Object.keys(result)
-        .sort()
-        // .sort(function (n1, n2) {
-        //     var l1 = result[n1].length;
-        //     var l2 = result[n2].length;
-        //     return l1 - l2;
-        // })
-        .map(function (streetName) {
-            var testVenue = result[streetName][0];
-            return [
-                streetName,
-                getStreetName(testVenue),
-                result[streetName].length
-            ];
-        })
-    );
-    console.log(Object.keys(result).length, venues.length/Object.keys(result).length);
-    // console.log(result['K61623535'].map(function (venue) {
-    //     return venue.location.address;
-    // }));
+    var groupsByStreet = venues.reduce(groupByStreetName, {});
+    var groupsByStreetAndDistance = Object.keys(groupsByStreet).map(function (sc) {
+        return groupsByStreet[sc];
+    }).map(function (streetGroup) {
+        return streetGroup.reduce(groupByDistance, []);
+    }).reduce(function (groupsByStreetAndDistance, streetGroups) {
+        return groupsByStreetAndDistance.concat(streetGroups);
+    }, []);
+    console.log(groupsByStreetAndDistance.sort(function (g1, g2) {
+        return g1.length - g2.length;
+    }).map(function (g) {
+        var testVenue = g[0];
+        return [getStreetName(testVenue), g.length];
+    }));
+    console.log('groupsByStreetAndDistance', groupsByStreetAndDistance.length);
 }).always(function (p) {
     setTimeout(function () {
         process.exit(0);
