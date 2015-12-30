4eaf187c2ff0528153c7e7885eadddeec965492d
Vova Bilonenko <bilonenko.v@gmail.com>
2015-12-30 21:46:56 +0100

    find closest street linestring and visualize

--- apps/nde-cluster.js
+++ apps/nde-cluster.js
@@ -7,6 +7,7 @@ var program = require('commander');
 var Mongo = require('schema-check-mongo-wrapper');
 var connection = new Mongo.Connection('mongodb://localhost:27017/foursqare');
 var collection = connection.collection('venues');
+var chroma = require('chroma-js');
 
 /**
  * callback for program.option
@@ -18,12 +19,16 @@ function list(s) {
 program
     .option('--bbox <items>', 'Venues bbox, lat, lng', list)
     .option('--ways [string]', 'json with ways exported from OSM', String)
+    .option('--filter [string]', 'filter by venue name', null)
     .description('clusters venues');
 
 program.parse(process.argv);
 
 var elements = require(process.cwd() + '/' + program.ways).elements;
 
+/**
+ * @type {nodes: Node[], ways: Way[]}
+ */
 var nodesAndWays = elements.reduce(function (nodesAndWays, element) {
     if (element.type === 'node') {
         nodesAndWays.nodes.push(element);
@@ -36,10 +41,21 @@ var nodesAndWays = elements.reduce(function (nodesAndWays, element) {
     ways: []
 });
 
-var nodesById = nodesAndWays.nodes.reduce(function (nodesById, node) {
-    nodesById[node.id] = node;
-    return nodesById;
-}, {});
+/**
+ * @type {Object.<Node.id, Node>}
+ */
+ var nodesById = nodesAndWays.nodes.reduce(function (nodesById, node) {
+     nodesById[node.id] = node;
+     return nodesById;
+ }, {});
+
+ /**
+  * @type {Object.<Way.id, Way>}
+  */
+// var waysById = nodesAndWays.ways.reduce(function (wbd, way) {
+//     wbd[way.id] = way;
+//     return wbd;
+// }, {});
 
 /**
  * Osm json element
@@ -63,13 +79,31 @@ var nodesById = nodesAndWays.nodes.reduce(function (nodesById, node) {
 
 /**
  * @param {Way} way
+ * @param {Object} props
  * @return {LineString}
  */
-function wayToPolyLine(way) {
+function wayToPolyLine(way, props) {
+    props.way = way;
     return turf.linestring(way.nodes.map(function (nodeId) {
         var node = nodesById[nodeId];
         return [node.lon, node.lat];
-    }));
+    }), props);
+}
+
+/**
+ * @param {Venue} venue
+ * @return {Point}
+ */
+function getVenuePoint(venue) {
+    var location = venue.location;
+    return {
+        'type': 'Feature',
+        'properties': {},
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [location.lng, location.lat]
+        }
+    };
 }
 
 collection.find({
@@ -88,7 +122,59 @@ collection.find({
         }}
     ]
 }).toArray().then(function (venues) {
-    console.log(JSON.stringify(turf.featurecollection([wayToPolyLine(nodesAndWays.ways[0])])));
+    var colors = chroma
+        .cubehelix()
+        .scale()
+        .correctLightness()
+        .colors(100);
+    var polylines = nodesAndWays.ways.map(function (way) {
+        return wayToPolyLine(way, {
+            stroke: colors[Math.floor(Math.random() * 100)]
+        });
+    });
+    var features = [];
+    if (!program.filter) {
+        features = [].concat(polylines);
+    }
+
+    venues.forEach(function (venue) {
+        if (program.filter && !venue.name.match(program.filter)) {
+            return;
+        }
+        var minDistance = +Infinity;
+        var venuePoint = getVenuePoint(venue);
+        var closestPointOnLine = null;
+        var closestLine = null;
+        polylines.forEach(function (polyline) {
+            var pointOnLine = turf.pointOnLine(polyline, venuePoint);
+            var distance = turf.distance(venuePoint, pointOnLine);
+            if (distance < minDistance) {
+                minDistance = distance;
+                //TODO:building can have few edges
+                closestLine = polyline;
+                closestPointOnLine = pointOnLine;
+            }
+        });
+        //TODO: elaborate insertIndex
+        // var bearingsChange = closestLine.geometry.coordinates.map(function (coordinates) {
+        //     return turf.point(coordinates);
+        // }).map(function (point) {
+        //     return turf.bearing(point, closestPointOnLine);
+        // }).map(function (angle, k, bearing) {
+        //     if (k < bearing.length - 1) {
+        //         var nextAngle = bearing[k+1];
+        //         return Math.round(Math.abs(((360 + angle)%360) - ((360 + nextAngle)%360)));
+        //     }
+        // });
+        // var insertIndex = bearingsChange.indexOf(180);
+        venuePoint.properties['marker-color'] = closestLine.properties.stroke;
+        venuePoint.properties.title = JSON.stringify(closestLine.properties.way.tags);
+        features.push(venuePoint);
+        if (program.filter) {
+            features.push(closestLine);
+        }
+    });
+    console.log(JSON.stringify(turf.featurecollection(features)));
 }).done(function () {
     process.exit(0);
 });
