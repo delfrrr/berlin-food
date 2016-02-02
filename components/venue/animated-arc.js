2efc255bc07350cd9c7c2cacf098006af441e9b0
Vova Bilonenko <bilonenko.v@gmail.com>
2016-02-02 23:40:01 +0100

    arc animation

---
+++ components/venue/animated-arc.js
@@ -0,0 +1,44 @@
+/**
+ * react component for animated arc path
+ * @module component/venue/animated-arc
+ */
+'use strict';
+var React = require('react');
+var arc = require('d3-shape').arc();
+var d3i = require('d3-interpolate');
+var _ = require('lodash');
+var DURATION = 400; //ms
+
+var Component = React.createClass({
+    getInitialState: function () {
+        return this.props;
+    },
+    componentWillReceiveProps: function (newProps) {
+        var component = this;
+        var interpolator = d3i.interpolate(this.props, newProps);
+        var start = Date.now();
+        this._animation = function () {
+            var now = Date.now();
+            var t = (now - start) / DURATION;
+            if (t > 1) {
+                t = 1;
+            }
+            var state = interpolator(t);
+            component.setState(state);
+            if (t < 1) {
+                window.requestAnimationFrame(component._animation);
+            }
+        }
+        window.requestAnimationFrame(component._animation);
+    },
+    componentWillUnmount: function () {
+        this._animation = function () {}
+    },
+    render: function () {
+        var pathProps = _.clone(this.state);
+        pathProps.d = arc(pathProps);
+        return React.DOM.path(pathProps);
+    }
+});
+
+module.exports = React.createFactory(Component);
--- components/venue/circle.js
+++ components/venue/circle.js
@@ -5,8 +5,7 @@
 'use strict';
 var React = require('react');
 var classnames = require('classnames');
-// var reactTransition = React.createFactory(require('react-transition'));
-var arc = require('d3-shape').arc();
+var animatedArc  = require('./animated-arc');
 
 require('./circle.less');
 
@@ -14,9 +13,9 @@ var Component = React.createClass({
     render: function () {
         var value = this.props.value;
         var unknown = !value;
-        var size = Math.sqrt(0.5);
+        var size = -1;
         if (!unknown) {
-            size = Math.sqrt(this.props.value / this.props.maxValue);
+            size = this.props.value / this.props.maxValue;
         }
         return React.DOM.div(
             {
@@ -32,15 +31,13 @@ var Component = React.createClass({
                 {
                     viewBox: "0 0 100 100"
                 },
-                React.DOM.path({
+                animatedArc({
                     transform: 'translate(50, 50)',
-                    d: arc({
-                        innerRadius: 40,
-                        outerRadius: 42,
-                        startAngle: 0,
-                        endAngle: Math.PI * 2 * size * (-1)
-                    }),
-                    fill: this.props.color
+                    innerRadius: 40,
+                    outerRadius: 42,
+                    startAngle: 0,
+                    endAngle: Math.PI * 2 * size * (-1),
+                    fill: unknown? '#efefef' : this.props.color
                 })
             ),
             React.DOM.div(
