f7da143e3b8af05a62c6f650efa7078aecb29b1f
Vova Bilonenko <bilonenko.v@gmail.com>
2016-02-19 17:24:11 +0100

    show foof categories

--- components/cluster/food-rating.js
+++ components/cluster/food-rating.js
@@ -5,11 +5,26 @@
 'use strict';
 var React = require('react');
 var classnames = require('classnames');
-// require('./index.less');
+require('./food-rating.less');
+var chroma = require('chroma-js');
+/**
+ * @param {Number} rating
+ * @return {Sting} color
+ */
+var ratingColorScale = chroma
+    .scale([
+        '#00b551',
+        '#73cf42',
+        '#C5DE35',
+        '#FFC800',
+        '#FF9600',
+        '#FF6701',
+        '#E6092C'])
+    .domain([10, 4]);
+
 var Component = React.createClass({
     render: function () {
         var foodRatings = this.props.foodRatings;
-        console.log(foodRatings);
         return React.DOM.div(
             {
                 className: classnames(
@@ -18,11 +33,55 @@ var Component = React.createClass({
                 )
             },
             foodRatings.map(function (ratingItem, k) {
+                var color = ratingColorScale(ratingItem.rating + 2); //becouse 7 is max
+                var countBarLength = ratingItem.count * 10;
+                var ratingBarSize = 3 * ratingItem.rating || 3;
                 return React.DOM.div(
                     {
+                        className: 'food-rating__rating-item',
                         key: k
                     },
-                    ratingItem.category.shortName
+                    React.DOM.div(
+                        {
+                            className: 'food-rating__short-name'
+                        },
+                        ratingItem.category.shortName.split('/')[0].trim()
+                    ),
+                    React.DOM.div(
+                        {
+                            className: 'food-rating__bar'
+                        },
+                        React.DOM.div(
+                            {
+                                className: 'food-rating__count-bar',
+                                style: {
+                                    width: countBarLength,
+                                    backgroundColor: color
+                                }
+                            }
+                        ),
+                        (ratingItem.count > 1) && React.DOM.div(
+                            {
+                                className: 'food-rating__count-bar-value',
+                                style: {
+                                    left: countBarLength / 2
+                                }
+                            },
+                            ratingItem.count
+                        ),
+                        React.DOM.div(
+                            {
+                                className: 'food-rating__rating-bar',
+                                style: {
+                                    left: countBarLength,
+                                    width:   ratingBarSize,
+                                    height:  ratingBarSize,
+                                    backgroundColor: color
+                                }
+                            }
+                        )
+                    )
+
                 );
             })
         );
